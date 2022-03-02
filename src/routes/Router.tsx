bae41e896fab1014b6bb5df666200682f7f0a11c
codekyz <codekyz0111@gmail.com>
2022-03-02 18:02:44 +0900

    Setting gh-pages

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -6,7 +6,7 @@ import Chart from "./Chart";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Coins />} />
         <Route path="/:coinId" element={<Coin />}>
