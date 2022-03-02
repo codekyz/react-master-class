aa5ac4a86255de78818a9a9c36e50d8f766198a6
codekyz <codekyz0111@gmail.com>
2022-03-02 15:39:32 +0900

    Finished course

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  bgColor: "#2f3640",
+  textColor: "#f5f6fa",
+  accentColor: "#9c88ff",
+  liColor: "#ffffff",
+  detailTextColor: "#f5f6fa",
+};
+
+export const lightTheme: DefaultTheme = {
+  bgColor: "#f5f6fa",
+  textColor: "#1e272e",
+  accentColor: "#fbc531",
+  liColor: "#40739e",
+  detailTextColor: "#f5f6fa",
+};
